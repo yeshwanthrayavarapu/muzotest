213e4e8ea517bd15c434b107fd33d81837ac6305
jaspwr <40232406+jaspwr@users.noreply.github.com>
2025-02-18 18:02:10 +1100

    fix: remove unrealted files

--- src/app/feedback/page.tsx
+++
@@ -1,9 +0,0 @@
-import { QUESTIONS } from "./questions";
-import Feedback from "./components/Feedback";
-
-export default function FullFeedbackPage() {
-  return (
-    <Feedback questionList={QUESTIONS} />
-  )
-}
-
